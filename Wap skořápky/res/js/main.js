0bcd64bcfe2ee84c888b2d7c8eb9f2916a4a4b9d
Jindřich Šuba <76794262+Bushakpico@users.noreply.github.com>
2021-11-29 16:37:03 +0100

    Add files via upload

---
+++ Wap skořápky/res/js/main.js
@@ -0,0 +1,38 @@
+const img = document.getElementsByClassName("img");
+const marble = document.getElementById("marble");
+const button = document.getElementById("button");
+
+let counter = 0;
+
+window.onload = () => {
+    random();
+}
+
+button.onclick = () => {
+  if (counter > 0) {
+    window.location.reload();
+
+  } else {
+    random();
+  }
+};
+
+random = () => {
+    let rand = Math.floor(Math.random() * 10);
+    console.log(rand);
+    if (rand <= 3) {
+      marble.style.transform = `translateX(-30em)`;
+    } else if (rand > 2 && rand < 7) {
+      marble.style.transform = `translateX(0em)`;
+    } else {
+      marble.style.transform = `translateX(30em)`;
+    }
+    counter = 0;
+}
+
+[...img].forEach((img) => {
+  img.onclick = () => {
+    counter++;
+    img.style.transform = `translateY(-15em)`;
+  };
+});
